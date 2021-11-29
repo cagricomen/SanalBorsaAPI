ffe2755a37ebe69f8fddd2126fab8cec4b2bfe36
cagricomen <cagri.comen45@gmail.com>
2021-11-29 14:41:40 +0300

    Repositories Added

---
+++ SanalBorsaAPI.Core/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanalBorsaAPI.Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity:class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(IEnumerable<TEntity> entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+    }
+}
