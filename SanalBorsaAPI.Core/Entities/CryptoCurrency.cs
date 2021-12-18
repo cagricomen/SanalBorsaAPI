7e50161dae5c0dea0a49a41194ec7b5c141260e7
cagricomen <cagri.comen45@gmail.com>
2021-12-19 01:04:09 +0300

    added shortName column

--- SanalBorsaAPI.Core/Entities/CryptoCurrency.cs
+++ SanalBorsaAPI.Core/Entities/CryptoCurrency.cs
@@ -10,6 +10,7 @@ namespace SanalBorsaAPI.Core.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ShortName { get; set; }
         public decimal BuyPriceDolar { get; set; }
         public decimal BuyPriceTL { get; set; }
         public string MarketingValue { get; set; }
